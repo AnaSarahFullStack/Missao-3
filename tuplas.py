61fac2e797c68a89fcd4b9c543e84548de58799d
Ana Sarah Cassimiro <167821855+AnaSarahFullStack@users.noreply.github.com>
2025-09-17 19:32:29 -0300

    Create tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,17 @@
+# Criação da tupla com os valores iniciais
+primeira_tupla = (1, 2, 3, 4, "Olá, tupla")
+
+# Impressão do conteúdo da tupla
+print("Conteúdo da tupla:", primeira_tupla)
+
+# Utilização do método index para obter o índice do elemento 4
+indice_do_elemento_4 = primeira_tupla.index(4)
+print("Índice do elemento 4 na tupla:", indice_do_elemento_4)
+
+# Verificação se a tupla contém o elemento 3
+contém_elemento_3 = 3 in primeira_tupla
+print("A tupla contém o elemento 3:", contém_elemento_3)
+
+# Verificação se a tupla contém o elemento 33
+contém_elemento_33 = 33 in primeira_tupla
+print("A tupla contém o elemento 33:", contém_elemento_33)
